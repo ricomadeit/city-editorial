9db29b113b6ea696f5d2788708f3644d9fad36b9
Erik Halabi <eriknabilhalabi@gmail.com>
2022-04-15 16:22:18 -0600

    working slider

--- src/components/CitySlider.js
+++ src/components/CitySlider.js
@@ -6,19 +6,19 @@ function CitySlider() {
 
     const [current, setCurrent] = useState(navigation.cities[0].section)
 
+    // https://www.youtube.com/watch?v=nGdwRP9ZsA4
     function resize(e, section) {
         setCurrent(section)
         var marker = document.querySelector('#marker')
         marker.style.left = e.offsetLeft+"px";
         marker.style.width = e.offsetWidth+"px";
-        console.log(marker.style.left)
     }
 
     return (
         <>
             <nav className="nav">
                 {navigation.cities.map( (item, i) => {
-                    return <p className={current === item.section ? 'currentItem' : 'item'} id={item.section} key={i} onClick={(e) => resize(e.target, item.section)}>{item.label}</p>
+                    return <a href="#" className={current === item.section ? 'currentItem' : 'item'} id={item.section} key={i} onClick={(e) => resize(e.target, item.section)}>{item.label}</a>
                 })}
                 <div className="slider" id="marker"></div>
             </nav>
